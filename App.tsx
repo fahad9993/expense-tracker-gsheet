cebfa319ce84376877f3e7e3506517bb02033ef3
Fahad <fahad9993@gmail.com>
2024-11-11 13:10:18 +0600

    Add app.tsx

--- App.tsx
+++ App.tsx
@@ -1,20 +1,123 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Pressable,
+  Alert,
+  SafeAreaView,
+} from "react-native";
+import BankNoteCard from "@/components/BankNoteCard";
 
-export default function App() {
+export default function Index() {
+  const apiEndpoint = "https://expense-tracker-8dh8.onrender.com";
+  const [bankNotes, setBankNotes] = useState<number[]>([]);
+  const [quantities, setQuantities] = useState<number[]>([]);
+
+  // Fetch data from the backend server on mount
+  useEffect(() => {
+    const loadInitialQuantities = async () => {
+      try {
+        const response = await fetch(`${apiEndpoint}/fetchQuantities`);
+        const { bankNotes: fetchedBankNotes, quantities: fetchedQuantities } =
+          await response.json();
+
+        setBankNotes(fetchedBankNotes || []); // Set the bankNotes from the response
+        setQuantities(
+          fetchedQuantities && fetchedQuantities.length
+            ? fetchedQuantities
+            : fetchedBankNotes.map(() => 0)
+        );
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    loadInitialQuantities();
+  }, []);
+
+  const handleResetAll = () => {
+    setQuantities(bankNotes.map(() => 0));
+  };
+
+  const handleUpdateQuantity = (index: number, newQuantity: number) => {
+    setQuantities((prevQuantities) => {
+      const updatedQuantities = [...prevQuantities];
+      updatedQuantities[index] = newQuantity;
+      return updatedQuantities;
+    });
+  };
+
+  const handleUpdate = async () => {
+    try {
+      await fetch(`${apiEndpoint}/updateQuantities`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ quantities }),
+      });
+      // Show alert if update is successful
+      Alert.alert("Success!", "Quantities updated successfully.", [
+        { text: "Ok" },
+      ]);
+    } catch (error) {
+      console.error("Error updating quantities:", error);
+      // Show error alert if update fails
+      Alert.alert("Error", "Failed to update quantities. Please try again.", [
+        { text: "OK" },
+      ]);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>Cash in Hand</Text>
+      <Pressable
+        style={({ pressed }) => [styles.button, { opacity: pressed ? 0.7 : 1 }]}
+        onPress={handleResetAll}
+      >
+        <Text style={{ color: "white", alignSelf: "center" }}>Reset all</Text>
+      </Pressable>
+      <FlatList
+        data={bankNotes}
+        keyExtractor={(item) => item.toString()}
+        renderItem={({ item, index }) => (
+          <BankNoteCard
+            note={item}
+            quantity={quantities[index]}
+            onUpdateQuantity={(newQuantity) =>
+              handleUpdateQuantity(index, newQuantity)
+            }
+          />
+        )}
+      />
+      <Pressable
+        style={({ pressed }) => [styles.button, { opacity: pressed ? 0.7 : 1 }]}
+        onPress={handleUpdate}
+      >
+        <Text style={{ color: "white", alignSelf: "center" }}>Update</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "green",
+    color: "white",
+    padding: 10,
+    marginTop: 20,
+    borderRadius: 10,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 20,
+  },
+  heading: {
+    fontSize: 20,
+    alignSelf: "center",
+    fontWeight: "bold",
   },
 });
